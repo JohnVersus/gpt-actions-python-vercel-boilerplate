72feb86864e4149e7395da97c61d1319ea9f6957
John Versus <johnvs.nagendra@gmail.com>
2023-07-12 21:58:58 +0530

    test: fastAPI deploy 🧪🧪

--- api/index.py
+++ api/index.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from .routes import router as NoteRouter
 
 app = FastAPI()
 
-@app.get("/api/python")
-def hello_world():
-    return {"message": "Hello World"}
+
+@app.get("/", tags=["Root"])
+async def read_root() -> dict:
+    return {
+        "message": "Welcome to my notes application, use the /docs route to proceed"
+    }
+
+
+app.include_router(NoteRouter, prefix="/note")

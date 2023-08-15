357921e483d25b3571e714c1d6c0de382e82acd2
John Versus <johnvs.nagendra@gmail.com>
2023-08-15 18:37:18 +0530

    postgres to sql db transfer 💾

--- api/database.py
+++ api/database.py
@@ -6,6 +6,54 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+# get connection details from environment variables
+ssl_args = {"ssl": {"verify_cert": False}}
+USERNAME = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWORD")
+HOST = os.getenv("DB_HOST")
+DATABASE = os.getenv("DB_NAME")
+
+# setup engine
+DATABASE_URL = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}"
+engine = create_engine(DATABASE_URL, future=True, connect_args=ssl_args)
+
+
+Session = sessionmaker(bind=engine, future=True)
+
+# setup base class for declarative syntax
+Base = declarative_base()
+
+
+class APIRequest(Base):
+    __tablename__ = "PluginApiRequests"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    query_parameters = Column(Text)
+    response_status = Column(Integer)
+    timestamp = Column(DateTime, default=datetime.utcnow)
+    subdivision_code = Column(String(255))  # Example length
+    host = Column(String(255))
+    real_ip = Column(String(255))
+    user_id = Column(String(255))
+    conversation_id = Column(String(255))
+    endpoint = Column(String(255))
+
+
+# create tables if they don't exist
+Base.metadata.create_all(bind=engine)
+
+
+# postgres code for ref
+"""
+from sqlalchemy import Column, String, Integer, DateTime, Text, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 # get connection details from environment variables
 USERNAME = os.getenv("PGUSER")
 PASSWORD = os.getenv("PGPASSWORD")
@@ -39,3 +87,4 @@ class APIRequest(Base):
 
 # create tables if they don't exist
 Base.metadata.create_all(bind=engine)
+"""
